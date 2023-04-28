095e544c3e74debb37c65425a2a9d51e40380261
Wallace Henrique <wallacehenriquerjbr@gmail.com>
2023-04-28 19:46:31 -0300

    Adição de lista suspensa e botão no formulário

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,36 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault();
+        console.log('Form submetido')
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto label="Nome" placeholder="Informe seu nome" />
-                <CampoTexto label="Cargo" placeholder="Informe seu cargo" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Informe seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Informe seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,19 @@
+import './ListaSuspensa.css'
+
+const ListaSuspensa = (props) => {
+    console.log(props.itens)
+
+    return (
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select required={props.obrigatorio}>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
+                <option></option>
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
